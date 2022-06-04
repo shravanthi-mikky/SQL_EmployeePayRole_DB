787a1446b990e49a37a53f0cc7db32c573cb4a5e
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-06-04 10:28:40 +0530

    [shravanthi] -Insert data into employee payroll table

--- PayrollServiceDB.sql
+++ PayrollServiceDB.sql
@@ -1,3 +1,4 @@
 create database Payroll_Service
 use Payroll_Service
 create table employee_payroll(ID int identity(1,1) primary key,Name varchar(100),Salary int,Start_Date date)
+INSERT into employee_payroll values ('Shravanthi',10000,'2022-01-03'),('Vishnu',20000,'2022-01-02'),('Bhagya',30000,'2022-01-01')
